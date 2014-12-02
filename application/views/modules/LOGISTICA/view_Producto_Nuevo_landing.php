a716d87ffa00dce4519ec5aaa86e80f8b62fc013
Gianpiere <jgianpiere@gmail.com>
2014-12-02 17:31:20 -0500

    maquetacion Agregar Producto
    maquetacion agregar producto parte 01

--- application/views/modules/LOGISTICA/view_Producto_Nuevo_landing.php
+++ application/views/modules/LOGISTICA/view_Producto_Nuevo_landing.php
@@ -1,1 +1,232 @@
-creacion de productos muy pronto.. en este canal ;)
+
+        <?= isset($this->RutaGuia) && !empty($this->RutaGuia) ? $this->RutaGuia : '';?>
+            <style type="text/css">
+                .fila:hover{
+                    background: rgba(200, 2, 2, 0.2);
+                }
+            </style>
+            <!-- tabs bar -->
+            <ul id="tab" class="nav nav-tabs">
+                <li class="active"><a href="#tab_01" data-toggle="tab" style="min-width:140px;margin-right:10px;">Agregar</a></li>
+            </ul>
+            <!-- tabs bar -->
+
+            <!-- tabls content bar -->
+            <div id="myTabContent" class="tab-content">
+                <div class="tab-pane fade active in" id="tab_01">
+                    <div style="padding-top:15px;" >
+                        <div class="row tab-pane-title col-xs-12 col-sm-12 col-md-12 col-lg-12">Agregar Producto</div>
+                            <!-- Formulario -->
+                            <div class="content">
+                                <div class="row">
+                                    <form action="" method="POST" id="form_agregar_producto" name="form_agregar_producto">
+                                    	<div class="form-group">
+                                            <label class="control-label col-lg-2 col-sm-5 col-md-3 col-xs-12 mtop-7">Codigo :</label>
+                                            <div class="col-lg-4 col-sm-6 col-md-3 col-xs-12 mtop-7">
+                                                <input type="text" placeholder="" name="agre_codigo" id="agre_codigo" class="form-control">
+                                            </div>
+
+                                            <label class="control-label col-lg-2 col-sm-5 col-md-3 col-xs-12 mtop-7">Codigo Claro :</label>
+                                            <div class="col-lg-4 col-sm-6 col-md-3 col-xs-12 mtop-7">
+                                                <input type="text" placeholder="" name="agre_codigo_claro" id="agre_codigo_claro" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-lg-9 col-sm-5 col-md-3 col-xs-12 mtop-7">Descripción :</label>
+                                            <div class="col-lg-4 col-sm-6 col-md-3 col-xs-12 mtop-7">
+                                                <input type="text" placeholder="" name="agre_descripcion" id="agre_descripcion" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-lg-9 col-sm-5 col-md-3 col-xs-12 mtop-7">Desp. Resumida :</label>
+                                            <div class="col-lg-4 col-sm-6 col-md-3 col-xs-12 mtop-7">
+                                                <input type="text" placeholder="" name="agre_desp_resumida" id="agre_desp_resumida" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div style="margin-bottom:30px;" class="row tab-pane-title col-xs-12 col-sm-12 col-md-12 col-lg-12">---</div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3">Clasificacion 1 :</label>
+                                            <div class="col-xs-3">
+                                                <select class="form-control">
+                                                    <option>-</option>
+                                                </select>
+                                            </div>
+
+                                            <label class="control-label col-xs-3">Clasificacion 2 :</label>
+                                            <div class="col-xs-3">
+                                                <select class="form-control">
+                                                    <option>-</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3">Marca :</label>
+                                            <div class="col-xs-3">
+                                                <select class="form-control">
+                                                    <option>-</option>
+                                                </select>
+                                            </div>
+
+                                            <label class="control-label col-xs-3">Modelo :</label>
+                                            <div class="col-xs-3">
+                                                <select class="form-control">
+                                                    <option>-</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3">Und. Med. Venta :</label>
+                                            <div class="col-xs-3">
+                                                <select class="form-control">
+                                                    <option>-</option>
+                                                </select>
+                                            </div>
+
+                                            <label class="control-label col-xs-3">Und. Med. Compra :</label>
+                                            <div class="col-xs-3">
+                                                <select class="form-control">
+                                                    <option>-</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div style="margin-bottom:30px;" class="row tab-pane-title col-xs-12 col-sm-12 col-md-12 col-lg-12">Agregar Almacen : <a href="#" class="btn-mas" id="btn-additem"></a></div>
+										
+										<div class="content">
+			                                <div class="row">
+			                                    <div class="container-fluid">
+			                                        <div style="margin-bottom:30px;" class="row tab-pane-title col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			                                            <div class="col-xs-1">#</div>
+			                                            <div class="col-xs-2">Codigo</div>
+			                                            <div class="col-xs-3">Descripción</div>
+			                                            <div class="col-xs-2">Cantidad</div>
+			                                            <div class="col-xs-2">Costo</div>
+			                                            <div class="col-xs-2">Total</div>
+			                                        </div>
+			                                        <div id="addItemsOrdenesdeCompra">
+			                                            <!-- <div item class="form-group">
+			                                                <div class="col-xs-1 text-center">1.- </div>
+			                                                <div class="col-xs-2"><input name="codigo" class="form-control" type="text" placeholder="Codigo" /></div>
+			                                                <div class="col-xs-3"><input name="descri" class="form-control" type="text" placeholder="Descripción" /></div>
+			                                                <div class="col-xs-2"><input calc-cant name="cantid" class="form-control" type="number" placeholder="Cantidad" /></div>
+			                                                <div class="col-xs-2"><input calc-prec calc="" name="precio" class="form-control" type="number" placeholder="Precio" /></div>
+			                                                <div class="col-xs-2"><input calc-tota name="total" class="form-control pull-right " type="number" placeholder="Total" /></div>
+			                                            </div> -->
+			                                        </div>
+
+			                                        <!-- <div class="form-group">
+			                                            <label class="control-label col-xs-10">TOTAL :</label>
+			                                            <div class="col-xs-2">
+			                                                <input type="text" class="form-control" id="agre_Total_OrdenCompra_total" name="agre_Total_OrdenCompra_total" placeholder="Total" required all-total validate="float" readonly="" value="0">
+			                                            </div>
+			                                        </div> -->
+			                                    </div>
+			                                </div>
+			                            </div>
+
+			                            <div style="margin-bottom:30px;" class="row tab-pane-title col-xs-12 col-sm-12 col-md-12 col-lg-12">-</div>
+
+			                            <div class="form-group">
+                                            <label class="control-label col-xs-3 text-right">Venta : <input type="checkbox" value="0" active-to="#agre_modalidad_OrdenCompra" name="agre_credito_OrdenCompra" id="agre_credito_OrdenCompra"></label>
+                                            <label class="control-label col-xs-3 text-right">Compra : <input type="checkbox" value="0" active-to="#agre_modalidad_OrdenCompra" name="agre_credito_OrdenCompra" id="agre_credito_OrdenCompra"></label>
+                                            <label class="control-label col-xs-3 text-right">Inventario : <input type="checkbox" value="0" active-to="#agre_modalidad_OrdenCompra" name="agre_credito_OrdenCompra" id="agre_credito_OrdenCompra"></label>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3">Costo Promedio :</label>
+                                            <div class="col-xs-3">
+                                                <input type="text" required="" placeholder="" name="" id="" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3 text-right">Activo : <input type="checkbox" value="0" active-to="#agre_modalidad_OrdenCompra" name="agre_credito_OrdenCompra" id="agre_credito_OrdenCompra"></label>
+                                        </div>
+
+                                        <div class="form-group mtop-7">
+                                            <div class="col-lg-offset-2 col-lg-10 col-md-12 col-sm-12 col-xs-12 mtop-7">
+                                                <input type="reset" class="btn btn-reset" value="Limpiar">
+                                                <input type="submit" class="btn btn-send" value="Guardar">
+                                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- Formulario -->
+                    </div>
+                </div>
+          </div>
+
+<script type="text/javascript">  
+
+
+    (function(){
+        $('form#form_agregar_producto').on('submit',function(e){ $$ = this; $this = $(this); $form = $this; e.preventDefault();
+            if(1==1){
+                $.ajax({
+                    type    : 'POST',
+                    data    : $form.serialize(),
+                    url     : "<?=site_url('/Logistica/Logistica/Entrada/Buscar');?>",
+                    success : function(data){
+                        try{
+                            $data = $.parseJSON(data);
+                            if($data[0] != 'ERROR' && $data[0] != '00'){
+                                if($data[0] == 'OK'){
+                                    $('.result').html($data[2]);
+                                    $('.header').show(0);
+
+                                    // Activar editar.
+                                    $('.result .fila').off('dblclick').on('dblclick',function(){
+                                        alert('cargar el editar');
+                                    });
+                                }
+                            }else{
+                                alert($data[2]);
+                            }
+                        }catch(e){
+                            console.log(data);
+                        }
+                        console.log(data);
+                    },
+                    error   : function(){
+
+                    }
+                });
+            }
+        });
+
+    
+
+</script>
+        
+<script>
+    (function(){ // reset items delete
+        $('.btn-reset').on('click',function(e){ $$ = this; $this = $(this);
+            $form = $($this.parent().parent().parent().parent().parent().parent());
+            if(confirm('deseas eliminar los items creados')){
+                $('#addItemsOrdenesdeCompra [item]').remove();
+            }else{
+                $listitems = $('#addItemsOrdenesdeCompra').find('input');
+                for(i in $listitems){ $item = $($listitems[i-1]);
+                    $item.data('data-value',$item.val());
+                }
+
+                $form[0].reset();
+
+                for(i in $listitems){ $item = $($listitems[i-1]);
+                    $item.val($item.data('data-value'));
+                }
+                
+            }
+
+            return false;
+        });
+    })(jQuery);
+</script>
