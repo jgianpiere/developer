edaddc67fd04861e519eb9953c39590cdabc2dce
Gianpiere <jgianpiere@gmail.com>
2014-12-02 11:52:59 -0500

    prueba popup comprobante compra
    iniciamos la prueba del popup en comprobante de compra para agregar
    proveedor.

---
+++ application/views/template/theme/footer.php
@@ -0,0 +1,24 @@
+<script>
+	function centerModal() {
+	    $(this).css('display', 'block');
+	    var $dialog = $(this).find(".modal-dialog");
+	    var offset = ($(window).height() - $dialog.height()) / 2;
+	    // Center modal vertically in window
+	    $dialog.css("margin-top", offset);
+	}
+
+	$('.modal').on('show.bs.modal', centerModal);
+	$(window).on("resize", function () {
+	    $('.modal:visible').each(centerModal);
+	});
+</script>
+
+<div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-body">
+            <img src="//placehold.it/1000x600" class="img-responsive">
+        </div>
+    </div>
+  </div>
+</div>
