4ce1216cf9bae40ed2e32db429c23cfe49b87386
Gianpiere <jgianpiere@gmail.com>
2014-12-02 12:03:05 -0500

    agrega enlace newProveedor para todos.
    todos los iconos enlazan a nuevo proveedor funcionando ok

--- application/views/template/theme/footer.php
+++ application/views/template/theme/footer.php
@@ -13,7 +13,7 @@
 	});
 </script>
 
-<div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div id="newProveedor" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-body">
