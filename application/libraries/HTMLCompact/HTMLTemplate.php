305c06a38a727ebb630e4d18fbd06bd6abb5ddf7
Gianpiere <jgianpiere@gmail.com>
2014-12-03 15:29:55 -0500

    ca
    ca

--- application/libraries/HTMLCompact/HTMLTemplate.php
+++ application/libraries/HTMLCompact/HTMLTemplate.php
@@ -448,7 +448,7 @@ class HTMLTemplate{
             /*echo json_encode($Params['Hijos']);*/
             foreach ($Params['Padres'] as $key => $padre) {
                 $HTML .= '<optgroup label="'.$padre[1].'">';
-                    $HTML .=  join("",$Params['Hijos'][$padre[0]]);
+                    $HTML .=  join('-',join("|",$Params['Hijos'][$padre[0]]));
                 $HTML .= '</optgroup>';
             }
         endif;
